7087ecda079d88c0b888998112a9344a78a66d54
Víctor Luque Martín <victorluque341@gmail.com>
2020-02-05 12:13:18 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "20px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "50px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
+
+document.getElementById("album").style.overflowY = "hidden";
+document.getElementById("album").style.overflowX = "scroll";
